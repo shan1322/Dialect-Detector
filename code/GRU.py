5e9b9bda4f7809b1180031f43ff775cd20780ab5
Shantanu Singh <shaneyasingh13@gmail.com>
2018-08-22 12:03:54 +0530

    re sampled

--- code/GRU.py
+++ code/GRU.py
@@ -14,7 +14,6 @@ y_train = np.load('../MAT/train_y.npy')
 y_test= np.load('../MAT/train_y.npy')
 x_val=np.load('../MAT/val_x.npy')
 y_val=np.load('../MAT/val_y.npy')
-
 print(x_train.shape)
 print(x_test.shape)
 print(y_train.shape)
@@ -25,17 +24,19 @@ print(y_val.shape)
 def GRUmodel(x_train):
     model = Sequential()
     model.add(GRU(64, activation='tanh',return_sequences=True, stateful=False,batch_input_shape= (batch_size, x_train.shape[1],x_train.shape[2] )))   #  timestep,datadim
-    model.add(GRU(64, activation='tanh',return_sequences=True, stateful=False))
-    model.add(GRU(64,activation='tanh', stateful=False))
-    model.add(Dropout(.25))
+    model.add(GRU(128, activation='tanh',return_sequences=True, stateful=False))
+    model.add(GRU(128, activation='tanh',return_sequences=True, stateful=False))
+    model.add(GRU(256, activation='tanh',return_sequences=True, stateful=False))
+    model.add(GRU(256,activation='tanh', stateful=False))
+    #model.add(Dropout(.25))
     model.add(Dense(nb_classes, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=["accuracy"])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
     return model
 # define paramaetes
-batch_size = 25
+batch_size = 100
 nb_classes = 214
 # train step
 model=GRUmodel(x_train)
-model.fit(x_train, y_train, batch_size=batch_size, epochs=1000,validation_data=(x_val, y_val))
+model.fit(x_train, y_train, batch_size=batch_size, epochs=20)
 # save model
 model.save('../Models/GRU.h5')
--- code/preprocess.py
+++ code/preprocess.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import pandas as pd
 import scipy.io.wavfile as wav
@@ -15,10 +9,6 @@ from keras.utils import np_utils
 from keras.preprocessing.sequence import TimeseriesGenerator
 import math
 from keras.preprocessing.sequence import pad_sequences
-
-
-# In[2]:
-
 
 # Load dataset
 df=pd.read_csv('../speakers_all.csv')
@@ -32,80 +22,32 @@ x_train,y_train,x_test,y_test=[],[],[],[]
 token=[]
 nat=df['native_language']
 nat=np.array(nat)
-print(nat)
-
-
-# In[3]:
-
-
 for i in range(0,len(native)):
     token.append(i)
 native=np.array(native)
-
-
-# In[4]:
-
-
 for i in tqdm(range(len(df))):
     try:
         fi=(path+filename[i]+".wav")
-        rate,data=librosa.load(fi)
-        mfcc = librosa.feature.mfcc(y=rate,sr=data, n_mfcc=20)
+        data,rate=librosa.load(fi)
+        data=data[len(data)-30000:len(data)]
+        mfcc = librosa.feature.mfcc(y=data,sr=rate, n_mfcc=20)
         for j in range(0,len(native)):
             if(nat[i]==native[j]):
                 y_train.append(token[j])
                 x_train.append(mfcc)
-
+               
     except:
-        1
-
-
-# In[6]:
-
-
-temp=[]
-for j in x_train:
-    temp.append(j.shape[1])
-print(int((min(temp)+max(temp))/2))
-
-
-# In[7]:
-
-
-X_train=[]
-for i in x_train:
-    X_train.append(pad_sequences(i, maxlen=2634))
-
-
-# In[8]:
-
-
-X_train=np.array(X_train)
-
-
-# In[9]:
-
-
-X_train.shape
-
-
-# In[10]:
-
-
+        print("error")
+x_train=np.array(x_train)
+print(x_train.shape)
 y=np_utils.to_categorical(y_train, 214)
 y_train,y_test,y_val=y[0:1800],y[1800:1937],y[1937:2137]
-x_train,x_test,x_val=X_train[0:1800],X_train[1800:1937],X_train[1937:2137]
+x_train,x_test,x_val=x_train[0:1800],x_train[1800:1937],x_train[1937:2137]
 print(x_train.shape,x_test.shape,x_val.shape)
 print(y_train.shape,y_test.shape,y_val.shape)
-
-
-# In[12]:
-
-
-np.save('../MAT/train_x.npy', x_train) 
-np.save('../MAT/train_y.npy', y_train) 
-np.save('../MAT/test_x.npy', x_test) 
-np.save('../MAT/test_y.npy', y_test) 
-np.save('../MAT/val_x.npy', x_val) 
-np.save('../MAT/val_y.npy', y_val) 
-
+np.save('../MAT/train_x.npy', x_train)
+np.save('../MAT/train_y.npy', y_train)
+np.save('../MAT/test_x.npy', x_test)
+np.save('../MAT/test_y.npy', y_test)
+np.save('../MAT/val_x.npy', x_val)
+np.save('../MAT/val_y.npy', y_val)
